65c131d1d4f8053270f8d0883635d7333f2bc44a
VishalMisal03 <misalvishal449@gmail.com>
2025-07-22 20:33:33 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login,{Profile,Setting}from './UserComponent'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Import and Exporting Components</h1>
+      <Login/>
+      <Profile/>
+      <Setting/>
+    </div>
+  )
+}
+export default App
